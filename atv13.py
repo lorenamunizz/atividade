2330b441e20d7213c053c4175035b58ddeb4083e
lorenamunizz <lorenamuniz594@gmail.com>
2025-03-29 17:48:26 -0300

    Create atv13.py

---
+++ atv13.py
@@ -0,0 +1,4 @@
+salario = int(input("Informe o salario inicial:"))
+anos = int(input("Informe a quantidade de anos"))
+salario_atual = salario * anos *2
+print(f"O salario atual é: {salario_atual}")
