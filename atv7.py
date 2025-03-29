588d1aaaadd911106dbbca7dfebd88db86342e2b
lorenamunizz <lorenamuniz594@gmail.com>
2025-03-29 17:43:59 -0300

    Create atv7.py

---
+++ atv7.py
@@ -0,0 +1,7 @@
+numero = int(input("Informe um numero impar:"))
+anterior = numero - 1
+quadrado = anterior*anterior
+proximo_numero = numero + 2
+diferenca = quadrado - numero
+diferenca2 = proximo_numero - numero
+print(f"A diferença entre o quadrado do número anterior é: {diferenca}, e entre o número e o proximo número impar é: {diferenca2}. ")
