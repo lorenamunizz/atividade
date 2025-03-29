224b3738bb0b9aa08dbe0a4d6bd9b5be4fc73d55
lorenamunizz <lorenamuniz594@gmail.com>
2025-03-29 17:37:21 -0300

    Create atv.py

---
+++ atv.py
@@ -0,0 +1,11 @@
+nota1 = int(input())
+nota2 = int(input())
+nota3 = int(input())
+
+mediaar = (nota1 + nota2 + nota3)/2
+pond1 = (nota1 + nota2 + nota3)/7
+pond2 = (nota1 + nota2 + nota3)/5
+
+print(f"A media aritmetrica simples é: {mediaar}")
+print(f"A media ponderada considerando os pesos 2,2 e 3 é:{pond1}")
+print(f"A media ponderada considerando os pesos 1,2 e 3 é:{pond2}")
