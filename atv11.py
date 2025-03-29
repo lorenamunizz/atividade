bcb053819445672e3c6144ba27404cf0afdc9144
lorenamunizz <lorenamuniz594@gmail.com>
2025-03-29 17:46:49 -0300

    Create atv11.py

---
+++ atv11.py
@@ -0,0 +1,6 @@
+nome = input("Informe o nome de usuario:")
+senha = input("Informe uma senha:")
+if nome == senha:
+    print("Não foi possivel criar a conta, pois o nome e a senha são iguais.")
+else:
+    print("Conta criada.")
